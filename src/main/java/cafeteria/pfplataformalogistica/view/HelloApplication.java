54a346cfa714aa8667e2486e28d1b868f65cfc09
andrewandfer <fernelyandres13@gmail.com>
2025-10-04 19:15:21 -0500

    commit 1

--- src/main/java/cafeteria/pfplataformalogistica/view/HelloApplication.java
+++ src/main/java/cafeteria/pfplataformalogistica/view/HelloApplication.java
@@ -1,4 +1,4 @@
-package cafeteria.pfplataformalogistica;
+package cafeteria.pfplataformalogistica.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,8 @@ module cafeteria.pfplataformalogistica {
 
     opens cafeteria.pfplataformalogistica to javafx.fxml;
     exports cafeteria.pfplataformalogistica;
+    exports cafeteria.pfplataformalogistica.view;
+    opens cafeteria.pfplataformalogistica.view to javafx.fxml;
+    exports cafeteria.pfplataformalogistica.controller;
+    opens cafeteria.pfplataformalogistica.controller to javafx.fxml;
 }
