c151a8f80b8158ed55adf1bbb08f5fc9f9f97043
andrewandfer <fernelyandres13@gmail.com>
2025-10-05 16:18:51 -0500

    fix: se corrigen errores en las clases Administrador,Persona,producto,Repartidor y usuario ademas se implementa el patron singleton a Empresa

--- src/main/java/PFPlataformaLogistica/model/Empresa.java
+++ src/main/java/PFPlataformaLogistica/model/Empresa.java
@@ -1,4 +1,25 @@
 package PFPlataformaLogistica.model;
 
-public class Empresa {
+public final class Empresa {
+    private String nombre;
+    private static Empresa instancia;
+    private Empresa() {
+        this.nombre = "MiEmpresa";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public static Empresa getInstance() {
+        if (instancia == null) {
+            instancia = new Empresa();
+        }
+        return instancia;
+    }
 }
+
